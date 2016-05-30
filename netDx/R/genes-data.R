01e5047bd654f0720d541987e424c99116c3a665
Shraddha Pai <shraddha.pai@utoronto.ca>
2016-05-30 14:58:19 -0400

    doc for genes.rda

---
+++ netDx/R/genes-data.R
@@ -0,0 +1,9 @@
+#' Example gene coordinates
+#'
+#' @docType data
+#'
+#' @usage data(genes)
+#' 
+#' @format data.frame with RefSeq ID (name), chromosome (chrom),
+#' strand (strand), transcript start and end (txStart,txEnd) and
+#' HGNC symbol name (name2)

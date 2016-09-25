17db9d7219965adc925857a9e1ca28611a62ff8b
Shraddha Pai <shraddhapai.neuro@gmail.com>
2016-09-25 11:27:29 -0400

    now computes shortest path for all pairwise combinations of classes. Also computes sd of distance

--- netDx/R/compareShortestPath.R
+++ netDx/R/compareShortestPath.R
@@ -2,7 +2,7 @@
 #'
 #' @details Uses Dijkstra's algorithm for weighted edges. Pairwise nodes with
 #' infinite distances are excluded before computing average shortest path 
-#' for a network. This function requires the igraph package to be installed. 
+#' for a network. This function requires the igraph package to be installed.
 #' The showNetDist option requires the gplots package to be installed.
 #' @param net (data.frame) network on which to compute shortest path. 
 #' SOURCE, TARGET, WEIGHTS. 
@@ -33,14 +33,18 @@ compareShortestPath <- function(net,pheno,showNetDist=FALSE,verbose=TRUE) {
 		tmp <- mat[upper.tri(mat,diag=FALSE)]
 		idx <- which(is.infinite(tmp))
 		if (any(idx)) tmp <- tmp[-idx]
-		if (verbose) cat(sprintf("N=%i distances\n", length(tmp)))
-		mean(tmp,na.rm=TRUE)
+		if (verbose) cat(sprintf("\tN=%i distances\n", length(tmp)))
+
+		c(mean(tmp,na.rm=TRUE), sd(tmp,na.rm=TRUE))
 	}
 	
 	g <- igraph::graph_from_data_frame(net, vertices=pheno$ID)
 	d_overall <- igraph::shortest.paths(g,algorithm="dijkstra")
+	cat(sprintf("Overall: %i nodes\n",length(pheno$ID)))
+	tmp <- .getAvgD(d_overall)
 	if (verbose)
-		cat(sprintf("Overall shortest path = %2.3f\n",.getAvgD(d_overall)))
+		cat(sprintf("All-all shortest path = %2.3f (SD=%2.3f)\n",
+					tmp[1],tmp[2]))
 	if (showNetDist) {
 			gplots::heatmap.2(t(d_overall),trace='none',scale='none',
 			dendrogram='none',main="node-level shortest path")
@@ -58,9 +62,29 @@ compareShortestPath <- function(net,pheno,showNetDist=FALSE,verbose=TRUE) {
 			vertices=cl)
 		tmp <- igraph::shortest.paths(g2,algorithm="dijkstra")
 		dset[[curr_cl]] <- .getAvgD(tmp)
+		tmp <- dset[[curr_cl]]
 		if (verbose) 
-			cat(sprintf("\tShortest dist (%s-%s)= %2.3f\n", 
-						curr_cl,curr_cl,dset[[curr_cl]]))
+			cat(sprintf("\t%s-%s: Mean shortest = %2.3f (SD= %2.3f)\n", 
+						curr_cl,curr_cl,tmp[1],tmp[2]))
+	}
+
+	# now repeat for all pairwise classes
+	cpairs <- as.matrix(combinat::combn(cnames,2))
+	for (k in 1:ncol(cpairs)) {
+		type1 <- pheno$ID[which(pheno$GROUP %in% cpairs[1,k])]
+		type2 <- pheno$ID[which(pheno$GROUP %in% cpairs[2,k])]
+		idx <- which(net[,1] %in% type1 & net[,2] %in% type2)
+		idx2 <- which(net[,1] %in% type2 & net[,2] %in% type1)
+		idx <- c(idx,idx2)
+		g <- igraph::graph_from_data_frame(d=net[idx,])
+		tmp <- igraph::shortest.paths(g,algorithm="dijkstra")
+		cur <- sprintf("%s-%s\n", cpairs[1,k],cpairs[2,k])
+		dset[[cur]] <- .getAvgD(tmp)
+		dset[[curr_cl]] <- .getAvgD(tmp)
+		tmp <- dset[[curr_cl]]
+		if (verbose) 
+			cat(sprintf("\t%s-%s: Mean shortest = %2.3f (SD= %2.3f)\n", 
+						cpairs[1,k],cpairs[2,k],tmp[1],tmp[2]))
 	}
 
 	dset[["overall"]] <- .getAvgD(d_overall)

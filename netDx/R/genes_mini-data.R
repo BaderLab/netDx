e372c2d78a76f79c403f36c008cc4978ab42b83f
Shraddha Pai <shraddhapai.neuro@gmail.com>
2016-10-25 13:41:34 -0400

    updated name to avoid name clash with genes in netDx.examples
    Former-commit-id: 2a8236505230b2f2ae1e67930520b74cac945b66
    Former-commit-id: 128142168feab2b57a8e65d441cf46f6bcfa484f

--- netDx/R/genes_mini-data.R
+++ netDx/R/genes_mini-data.R
@@ -2,7 +2,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(genes)
+#' @usage data(genes_mini)
 #' 
 #' @format data.frame with RefSeq ID (name), chromosome (chrom),
 #' strand (strand), transcript start and end (txStart,txEnd) and

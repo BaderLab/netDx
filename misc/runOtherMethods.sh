d99741f1828ce4526fdde7a4f513d56314007c51
Shraddha Pai <shraddhapai.neuro@gmail.com>
2016-10-25 14:20:14 -0400

    now runs genes-in-pathways filter

--- misc/runOtherMethods.sh
+++ misc/runOtherMethods.sh
@@ -2,8 +2,10 @@
 # run other methods to compare with netdx
 
 # avg xpr by pathway
-Rscript enet_avgXprByPathway.R ElasticNet
-Rscript enet_avgXprByPathway.R RandomForest
-
+#Rscript enet_avgXprByPathway.R ElasticNet
+#Rscript enet_avgXprByPathway.R RandomForest
+# genes in pathways
+Rscript enet_genesInPathways.R ElasticNet
+Rscript enet_genesInPathways.R RandomForest
 
 

b8a038a3b8498b15ccd433cfeab95a56e808593d
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-01-17 12:50:56 +0000

    collect rng run results

---
+++ misc/xprCNV_collectRes.R
@@ -0,0 +1,23 @@
+#' collect results of brca simulations
+#'
+rm(list=ls())
+
+resDir <- "~/tmp/BRCA_xprCNV"
+#dirList <- dir(path=resDir,pattern="R")
+dirList <- paste("R",2:79,sep="")
+#dirList <- setdiff(dirList,c("R29","R134","R135"))
+
+outmat <- matrix(nrow=length(dirList),ncol=6)
+colnames(outmat) <- c("tp","fp","tn","fn","acc","ppv")
+rownames(outmat) <- dirList
+
+ctr <- 1
+for (d in dirList){
+        print(d)
+        load(sprintf("%s/%s/FinalResults.Rdata",resDir,d))
+        outmat[ctr,] <- unlist(out$testRes$perfStats)
+        ctr <- ctr+1
+}
+outFile <- sprintf("%s/VM_results.txt",resDir)
+write.table(outmat,file=outFile,sep="\t",col=T,row=T,quote=F)
+

28e3a6a35f234f222e036841c0f712f2cce28df6
Shraddha Pai <shraddhapai.neuro@gmail.com>
2017-02-03 10:42:18 -0500

    extended to multi rng-seed
    Former-commit-id: 03eccce32ee7cd4c0d6efd17d4cc159101255a03
    Former-commit-id: 964e9fc9a36b96132318e6a247f93d114f795ca1

--- misc/ADNI/ADNI_Q3_saveForTest.R
+++ misc/ADNI/ADNI_Q3_saveForTest.R
@@ -6,15 +6,18 @@ trainProp <- 0.67
 inFile <- "/home/spai/BaderLab/DREAM_AD/input/GuanLab/C3/ADNI_Training_Q3_new.csv_matlab.csv"
 outDir <- dirname(inFile)
 
-
 dat <- read.delim(inFile,sep=",",h=F,as.is=T)
 colnames(dat)[1] <- "ID"
 colnames(dat)[7] <- "STATUS"
-set.seed(102);
-TT_STATUS <- splitTestTrain(dat,pctT=trainProp)
+for (k in seq(5,100,5)) {
+		cat(sprintf("Seed = %i\n",k))
+TT_STATUS <- splitTestTrain(dat,pctT=trainProp,setSeed=k)
 idx <- which(TT_STATUS %in% "TRAIN")
-write.table(dat[idx,],sep=",",file=sprintf("%s.TRAIN.txt",inFile),col=F,row=F)
+write.table(dat[idx,],sep=",",file=sprintf("%s.TRAIN_%i.txt",inFile,k),
+			col=F,row=F)
 idx <- which(TT_STATUS %in% "TEST")
-write.table(dat[idx,],sep=",",file=sprintf("%s.TEST.txt",inFile),col=F,row=F)
+write.table(dat[idx,],sep=",",file=sprintf("%s.TEST_%i.txt",inFile,k),
+			col=F,row=F)
+}
 
 

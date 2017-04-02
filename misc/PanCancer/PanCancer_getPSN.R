d652a0009513a8e17f5e39dca13154345bb12af5
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-04-02 10:46:21 -0400

    Loops over all datasets/conditions. Automatically generates PSN in Cytoscape. working version
    Former-commit-id: 73b81f38cefce4f9360a03492f50b9a46d5c3fbe
    Former-commit-id: 0eb1a9b4233a4b87d496c4f14e0052af5d5bdc02

--- misc/PanCancer/PanCancer_getPSN.R
+++ misc/PanCancer/PanCancer_getPSN.R
@@ -1,6 +1,43 @@
 #' generate integrated PSN for TCGA breast cancer data
 rm(list=ls())
 
+# --------------------------------------------------------------
+# Param for computing integrated PSN
+outDir		<- "."
+cutoff 		<-10  	# include nets with score >= this value
+corrThresh 	<-0.7 	# exclude edges with similarity < this threshold
+dt <- format(Sys.Date(),"%y%m%d")
+
+# --------------------------------------------------------------
+# setup for network generation in Cytoscape
+require(EasycyRest) # https://github.com/shraddhapai/EasycyRest
+require(httr)
+
+styleName <- "PSNstyle"
+portNum <- 1234
+base.url <- sprintf("http://localhost:%i/v1",portNum)
+res	<- httr::GET(sprintf("%s/styles",base.url))
+curStyles <- gsub("\\\"","",rawToChar(res$content))
+curStyles <- unlist(strsplit(curStyles,","))
+
+if (any(grep("PSNstyle",curStyles))) {
+	cat("style exists not creating\n")
+} else {
+	cat("Creating style\n")
+	nodeFills <- map_NodeFillDiscrete("GROUP",
+		c("SURVIVEYES","SURVIVENO"),
+		c("#FF9900","#003399"))
+	defaults <- list("NODE_SHAPE"="ellipse",
+			"NODE_SIZE"=50,
+			"EDGE_TRANSPARENCY"=120,
+			"NODE_TRANSPARENCY"=120)
+	sty <- createStyle(styleName, 
+		defaults=defaults,
+		mappings=list(nodeFills))
+}
+
+# --------------------------------------------------------------
+# data dirs for input
 rootDir <- "/Users/shraddhapai/Documents/Research/BaderLab"
 # to create pheno table
 clinList <- list(
@@ -25,27 +62,36 @@ selIter <- list(
 							 rootDir)
 )
 
-curSet 				<- "LUSC"
-clinical_file <- clinList[[curSet]]
-survival_file <- survList[[curSet]]
-dataDir 			<- selIter[[curSet]]
-
-outDir		<- "."
-cutoff 			<-10  	# include nets with score >= this value
-corrThresh 	<-0.7 	# exclude edges with similarity lower than this threshold
-
 require(netDx)
 
-dt <- format(Sys.Date(),"%y%m%d")
-logFile <- sprintf("%s/%s_%s.log",outDir,curSet,dt)
+# --------------------------------------------------------------
+# Work begins
+
+logFile <- sprintf("%s/getPSN_%s.log",outDir,dt)
 sink(logFile,split=TRUE)
 
 tryCatch({
 # aggFun aggregate edges between any given pair of patients
 # simMode normal|BinProp. BinProp converges all binary sims into
 		# "proportion binary with similarity".
-for (aggFun in c("MAX","MEAN")) {
-	for (simMode in c("normal","BinProp")) {
+
+datSets <- c("OV","KIRC","LUSC","GBM")
+dijk <- list()
+for (aggFun in c("MAX")) {
+	dijk[[aggFun]] <- list()
+	for (simMode in "BinProp") { 		
+		curDijk <- matrix(NA,nrow=length(datSets),ncol=8)
+		rownames(curDijk) <- datSets
+		colnames(curDijk) <- c("NO","YES","YES-NO","overall","pYES-YESNO",
+			"pYES-overall","pNO-YESNO","pNO-overall")
+
+		cur_i <- 1
+
+		for (curSet in datSets) {		
+		clinical_file <- clinList[[curSet]]
+		survival_file <- survList[[curSet]]
+		dataDir <- selIter[[curSet]]
+
 		cat(sprintf("%s: %s: %s\n", curSet, simMode, aggFun))
 
 		# -----------------------------------------------------
@@ -90,23 +136,21 @@ for (aggFun in c("MAX","MEAN")) {
 		dir.create(poolDir)
 		
 		newNetIDs <- list()
+
+		# pool feature selected nets from both groups
 		for (gps in names(pTallyFile)) {
 			cat(sprintf("Group %s\n", gps))
 			pTally <- read.delim(pTallyFile[[gps]],sep="\t",h=T,as.is=T)
 			pTally <- subset(pTally, pTally[,2]>=cutoff)[,1]
-			
-		###	cat("****************************\n")
-		###	cat("*** Excluding MUT nets   ***\n")
-		###	cat("****************************\n")
-		###	pTally <- pTally[-grep("^MUT_|stage|grade",pTally)]
-		
 			pTally <- sub("_cont|\\.profile","",pTally)
-		
+			if ("BRCA2" %in% pTally) pTally <- pTally[-which(pTally=="BRCA2")]
+
 			netInfo_cur <- read.delim(netInfo[[gps]],sep="\t",h=F,as.is=T)
 			netInfo_cur[,2] <- sub("_cont|\\.profile","",netInfo_cur[,2])
-			# copy feature selected nets for this group
 			curNetIds <- matrix(NA,nrow=length(pTally),ncol=2)
 			ctr <- 1
+
+			# copy feature selected nets for this group
 			for (cur in pTally) {
 				idx <- which(netInfo_cur[,2] == cur)
 				if (length(idx)<1) {
@@ -120,32 +164,39 @@ for (aggFun in c("MAX","MEAN")) {
 					
 				curNetIds[ctr,] <- c(sub(".txt","",tmp), 
 									 paste(gps,netInfo_cur[idx,2],sep=".")) 
-				cat(sprintf("\t%s -> %s\n", cur, tmp))
+				##cat(sprintf("\t%s -> %s\n", cur, tmp))
 				ctr <- ctr+1
 			}
 			newNetIDs[[gps]] <- curNetIds
 		}
+
 		# write compiled net info file
 		newNetIDs <- do.call("rbind",newNetIDs)
 		if (simMode == "BinProp") {
 			isBinary <- rep(0,nrow(newNetIDs))
-			isBinary[grep("MUT_|stage|grade",newNetIDs[,2])] <- 1
+			isBinary[grep("MUT_|stage|grade|gender",newNetIDs[,2])] <- 1
 			newNetIDs <- cbind(newNetIDs, isBinary=isBinary)
 		}
 		netInfo_combinedF <- sprintf("%s/netInfo.txt", poolDir)
-		write.table(newNetIDs,file=netInfo_combinedF,sep="\t",col=F,row=F,quote=F)
+		write.table(newNetIDs,file=netInfo_combinedF,sep="\t",
+			col=F,row=F,quote=F)
 		
-		# now create integrated net using the best nets pooled from all classes
+		# now create integrated net using the best nets pooled 
+		# from all classes
 		cat(sprintf("%i networks with score >=%i\n",nrow(pTally),cutoff))
 		cat("-----\n")
+pdf("test.pdf")
 		aggNetFile <- netDx::writeWeightedNets(ptFile$YES,
 						netInfo=netInfo_combinedF,
 						poolDir,keepNets=newNetIDs[,2],outDir,
 						filterEdgeWt=corrThresh,limitToTop=25,
-						outFileName=sprintf("%s_%s_%s_PSN.txt", curSet,simMode,aggFun),
-						writeAggNet=aggFun,verbose=TRUE)
+						outFileName=sprintf("%s_%s_%s_PSN.txt", 
+							curSet,simMode,aggFun),
+						writeAggNet=aggFun,verbose=FALSE)
+dev.off()
 		aggNet<- read.delim(aggNetFile,sep="\t",h=T,as.is=T)[,1:3]
-		colnames(aggNet)[3] <- "weight"
+		colnames(aggNet) <- c("AliasA","AliasB","weight")
+		aggNet[,3] <- 1-aggNet[,3]  # convert similarity to dissimilarity
 		
 		# need pheno table for class assignment.
 		pheno <- read.delim(clinical_file,sep="\t",h=T,as.is=T)
@@ -157,28 +208,39 @@ for (aggFun in c("MAX","MEAN")) {
 		survStr[surv$STATUS_INT>0] <- "SURVIVEYES"
 		surv$STATUS <- survStr
 		pheno <- merge(x=pheno,y=surv,by="ID")
+
 		pheno$X <- NULL
-		pheno$gender <- ifelse(pheno$gender=="FEMALE",1, 0)
 		
 		colnames(pheno)[which(colnames(pheno)=="STATUS")] <- "GROUP"
-		aggNet[,3] <- 1-aggNet[,3]  # convert similarity to dissimilarity
 		cat("\n\n---------------------\n")
-		cat(sprintf("Dijkstra distances: %s: %s :%s\n",curSet,aggFun,simMode))
+		cat(sprintf("Dijkstra distances: %s: %s :%s\n",
+			curSet,aggFun,simMode))
 		cat("---------------------\n")
-		x <- compareShortestPath(aggNet, pheno)
-		pyes <- wilcox.test(x$all[["SURVIVEYES"]],x$all[["SURVIVEYES-SURVIVENO"]],
-										 alternative="less")$p.value
-		pno <- wilcox.test(x$all[["SURVIVENO"]],x$all[["SURVIVEYES-SURVIVENO"]],
-										 alternative="less")$p.value
-		pyes2 <- wilcox.test(x$all[["SURVIVEYES"]],x$all[["overall"]],
-										 alternative="less")$p.value
-		pno2 <- wilcox.test(x$all[["SURVIVENO"]],x$all[["overall"]],
-										 alternative="less")$p.value
-		cat("WMW tests (one-sided, less)\n")
-		cat(sprintf("YES: -> yes-no: p %1.2e ; -> ALL: p%1.2e\n", pyes,pyes2))
-		cat(sprintf("NO: -> yes-no: p %1.2e ; -> ALL: p%1.2e\n", pno,pno2))
+		x <- compareShortestPath(aggNet, pheno,verbose=F)
+		###print(lapply(x$avg, function(k) k[1]))
 		
-		#### plot density of shortest distances and compute statistics on significance
+		pyes <- wilcox.test(x$all[["SURVIVEYES"]],
+				x$all[["SURVIVEYES-SURVIVENO"]],
+				alternative="less")$p.value
+		pno <- wilcox.test(x$all[["SURVIVENO"]],
+				x$all[["SURVIVEYES-SURVIVENO"]],
+				 alternative="less")$p.value
+		pyes2 <- wilcox.test(x$all[["SURVIVEYES"]],x$all[["overall"]],
+				alternative="less")$p.value
+		pno2 <- wilcox.test(x$all[["SURVIVENO"]],x$all[["overall"]],
+				alternative="less")$p.value
+		cat("WMW tests (one-sided, less)\n")
+		cat(sprintf("YES: -> yes-no: p %1.2e ; -> ALL: p%1.2e\n", 
+			pyes,pyes2))
+		cat(sprintf("NO: -> yes-no: p %1.2e ; -> ALL: p%1.2e\n", 
+			pno,pno2))
+
+		curDijk[cur_i,] <- c(unlist(lapply(x$avg,function(k) k[1])),
+			pyes,pyes2,pno,pno2)
+		cur_i <- cur_i+1
+		
+		# plot density of shortest distances and compute i
+		# statistics on significance
 		if (!aggFun == "MAX") {
 		pdf(sprintf("%s/%s_%s_%s_shortestDist.pdf",
 						outDir, curSet,aggFun,simMode),width=8,height=6)
@@ -221,12 +283,33 @@ for (aggFun in c("MAX","MEAN")) {
 			dev.off()
 		})
 		}
-		
 		write.table(pheno,file=sprintf("%s/%s_%s_%s_PSN_pheno.txt",
-																	 outDir,curSet, simMode, aggFun),sep="\t",
-					col=T,row=F,quote=F)
+			 outDir,curSet, simMode, aggFun),sep="\t",col=T,row=F,quote=F)
+
+		# layout network in Cytoscape
+		network.suid <- EasycyRest::createNetwork(
+			nodes=pheno, nodeID_column="ID",edges=aggNet,
+				netName=sprintf("%s_%s_%s",curSet,simMode,aggFun),
+				collName=curSet
+		)
+		# spring-embedded layout on edge 'weight' column
+		layout.url <- sprintf("%s/apply/layouts/kamada-kawai/%s?column=weight",
+					base.url,network.suid, sep="/")
+		response <- httr::GET(url=layout.url) 
+		# apply style
+		apply.style.url <- sprintf("%s/apply/styles/%s/%i",
+					base.url,styleName,network.suid)
+		response <- httr::GET(apply.style.url)
+		
 	}
+	curDijk <- cbind(curDijk,method=sprintf("%s:%s", simMode,aggFun))
+	dijk[[sprintf("%s_%s", aggFun,simMode)]] <- curDijk
 }
+}
+
+dijk <- do.call("rbind",dijk)
+write.table(dijk,file=sprintf("Dijkstra_PSN_%s.txt",dt),sep="\t",col=T,row=T,
+	quote=F)
 },error=function(ex){ 
 	print(ex)
 }, finally={

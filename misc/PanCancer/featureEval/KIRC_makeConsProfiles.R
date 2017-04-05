f2346ef2b95bd113e03946dba4f9f152798491aa
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-04-05 09:33:09 -0400

    evaluate feature relevance to survival
    Former-commit-id: d80dd0cba5536e65bf1b7e5790f7d5429f9eb737
    Former-commit-id: 30712d21d7d0ee35249fabb82c4ce03d6d3ebaeb

---
+++ misc/PanCancer/featureEval/KIRC_makeConsProfiles.R
@@ -0,0 +1,214 @@
+#' feature selection for GBM from PanCancer survival dataset
+#' 10-fold CV predictor design with clinical and mRNA data
+rm(list=ls())
+require(netDx)
+require(netDx.examples)
+
+numCores <- 8L
+GMmemory <- 4L
+trainProp <- 0.8
+cutoff <- 9
+
+inDir <- "/home/ahmad/tcga_datasets/KIRC/input"
+outRoot <-"/home/spai/KIRC/output"
+
+if (!file.exists(outRoot)) dir.create(outRoot)
+
+dt <- format(Sys.Date(),"%y%m%d")
+megaDir <- sprintf("%s/featSel_%s",outRoot,dt)
+
+# ----------------------------------------------------------------
+# helper functions
+
+# normalized difference 
+# x is vector of values, one per patient (e.g. ages)
+normDiff <- function(x) {
+    #if (nrow(x)>=1) x <- x[1,]
+    nm <- colnames(x)
+    x <- as.numeric(x)
+    n <- length(x)
+    rngX  <- max(x,na.rm=T)-min(x,na.rm=T)
+    
+    out <- matrix(NA,nrow=n,ncol=n);
+    # weight between i and j is
+    # wt(i,j) = 1 - (abs(x[i]-x[j])/(max(x)-min(x)))
+    for (j in 1:n) out[,j] <- 1-(abs((x-x[j])/rngX))
+    rownames(out) <- nm; colnames(out)<- nm
+    out
+}
+
+# -----------------------------------------------------------
+# process input
+inFiles <- list(
+	clinical=sprintf("%s/KIRC_clinical_core.txt",inDir),
+	survival=sprintf("%s/KIRC_binary_survival.txt",inDir),
+	rna=sprintf("%s/KIRC_mRNA_core.txt",inDir),
+	mut=sprintf("%s/from_firehose/KIRC_core_somatic_mutations.txt",
+		inDir)
+)
+
+pheno <- read.delim(inFiles$clinical,sep="\t",h=T,as.is=T)
+colnames(pheno)[1] <- "ID"
+
+#======transform clinical data=========
+pheno$grade <- as.vector(pheno$grade)
+pheno$grade[pheno$grade=="G1"] <- "G2"
+pheno$grade[pheno$grade=="GX"] <- "G2"
+pheno$grade <- as.factor(pheno$grade)
+pheno <- pheno[, -which(colnames(pheno)=="gender")]
+#======================================
+
+
+surv <- read.delim(inFiles$survival,sep="\t",h=T,as.is=T)
+colnames(surv)[1:2] <- c("ID","STATUS_INT")
+survStr <- rep(NA,nrow(surv))
+survStr[surv$STATUS_INT<1] <- "SURVIVENO"
+survStr[surv$STATUS_INT>0] <- "SURVIVEYES"
+surv$STATUS <- survStr
+pheno <- merge(x=pheno,y=surv,by="ID")
+pheno$X <- NULL
+# pheno$gender <- ifelse(pheno$gender=="FEMALE",1, 0)
+pheno_nosurv <- pheno[1:4]
+
+dats <- list() #input data in different slots
+
+# clinical
+# cat("\t* Clinical\n")
+# clinical <- pheno_nosurv
+# rownames(clinical) <- clinical[,1]; 
+# clinical$ID <- NULL
+# clinical$performance_score[which(clinical$performance_score == "[Not Available]")] <- NA
+# clinical$performance_score <- strtoi(clinical$performance_score)
+# clinical <- t(clinical)
+# dats$clinical <- clinical; rm(clinical)
+cat("\t* Clinical\n")
+clinical <- pheno_nosurv
+rownames(clinical) <- clinical[,1];
+clinical$grade <- as.numeric(factor(clinical$grade))
+clinical$stage <- as.numeric(factor(clinical$stage))
+clinical$ID <- NULL
+clinical <- t(clinical)
+dats$clinical <- clinical; rm(clinical)
+
+# RNA
+cat("\t* RNA\n")
+rna <- read.delim(inFiles$rna,sep="\t",h=T,as.is=T)
+rna <- t(rna)
+colnames(rna) <- rna[1,]; rna <- rna[-1,]; 
+rna <- rna[-nrow(rna),]
+class(rna) <- "numeric"
+rownames(rna) <- sub("mRNA_","",rownames(rna))
+rownames(rna) <- sub("\\..*","",rownames(rna))
+dats$rna <- rna; rm(rna)
+
+# include only data for patients in classifier
+dats <- lapply(dats, function(x) { x[,which(colnames(x)%in%pheno$ID)]})
+dats <- lapply(dats, function(x) { 
+	midx <- match(pheno$ID,colnames(x))
+	x <- x[,midx]
+	x
+})
+
+# somatic mutations
+cat("\t* Somatic mutations\n")
+mut <- read.delim(inFiles$mut,sep="\t",h=T,as.is=T)
+# next steps: convert into GRanges with LOCUS_NAMES column.
+# call makePSN_NamedRanges() to make pathway-level nets.
+# run training.
+pat_GR <- GRanges(paste("chr",mut$Chromosome,sep=""),
+	IRanges(mut$Start_position,mut$End_position))
+pat_GR$LOCUS_NAMES<-mut$Hugo_Symbol
+pat_GR$ID <- mut$ID
+
+pheno_all <- pheno; 
+pat_GR_all <- pat_GR;
+
+rm(pheno,pheno_nosurv,pat_GR,mut)
+
+# ----------------------------------------------------------
+# build classifier
+numCores <- 8L
+if (file.exists(megaDir)) unlink(megaDir,recursive=TRUE)
+dir.create(megaDir)
+
+logFile <- sprintf("%s/log.txt",megaDir)
+sink(logFile,split=TRUE)
+tryCatch({
+outDir <- sprintf("%s/ConsNets", megaDir)
+
+	pheno_all$TT_STATUS <- "DUMMY" 
+	
+	# create nets 
+	netDir <- sprintf("%s/networks",outDir) 
+	pathFile <- sprintf("%s/extdata/Human_160124_AllPathways.gmt", 
+	   path.package("netDx.examples"))
+	pathwayList <- readPathways(pathFile)
+	data(genes)
+	gene_GR     <- GRanges(genes$chrom,IRanges(genes$txStart,genes$txEnd),
+	  	name=genes$name2)
+	cat("* Limiting to pathway genes\n")
+	path_GRList <- mapNamedRangesToSets(gene_GR,pathwayList)
+	names(path_GRList) <- paste("MUT_",names(path_GRList),sep="")
+	clinList <- list(age="age",grade="grade",stage="stage")
+	
+###	# run featsel once per subtype
+	subtypes <- unique(pheno_all$STATUS)
+	
+	## ----class-prediction, eval=TRUE-------------------------
+	# now create GM databases for each class
+	# should contain train + test patients
+	# and be limited to nets that pass feature selection
+	pheno <- pheno_all
+	predRes <- list()
+	for (g in subtypes) {
+		pDir <- sprintf("%s/%s",outDir,g)
+		# get feature selected net names
+		pTally <- read.delim(
+			sprintf("/home/spai/KIRC/KIRC_%s_consNets.txt", g),
+			sep="\t",h=T,as.is=T)
+		pTally <- pTally[,1]
+		pTally <- sub(".profile","",pTally)
+		pTally <- sub("_cont","",pTally)
+		cat(sprintf("%s: %i pathways\n",g,length(pTally)))
+		netDir <- sprintf("%s/networks",pDir)
+		if (!file.exists(netDir)) dir.create(netDir,recursive=TRUE)
+	
+        # prepare nets for new db
+        # RNA 
+        idx <- which(names(pathwayList) %in% pTally)
+        if (any(idx)) {
+            cat(sprintf("RNA: included %i nets\n", length(idx)))
+            tmp <- makePSN_NamedMatrix(dats$rna, rownames(dats$rna),
+                 pathwayList[idx],
+                netDir,verbose=F,numCores=numCores, writeProfiles=TRUE)
+        }
+            
+        # clinical
+		idx <- which(names(clinList) %in% pTally)
+		if (any(idx)) {
+            cat(sprintf("clinical: included %i nets\n", length(idx)))
+            netList2 <- makePSN_NamedMatrix(dats$clinical, rownames(dats$clinical),
+                clinList[idx],
+                netDir, simMetric="custom",customFunc=normDiff,
+                sparsify=TRUE,verbose=TRUE,numCores=numCores,append=TRUE)
+        }
+
+		# # add somatic mutations at pathway-level
+		# netList3 <- makePSN_RangeSets(pat_GR_all, path_GRList, netDir,
+			# numCores=numCores)
+            
+        # add somatic mutations at pathway-level
+		idx <- which(names(path_GRList) %in% pTally) 
+		if (any(idx)) {
+			cat(sprintf("mutations: included %i nets\n", length(idx)))
+			netList3 <- makePSN_RangeSets(pat_GR_all, 
+				path_GRList[idx], 
+				netDir,numCores=numCores)
+        }
+	}
+	
+}, error=function(ex){
+	print(ex)
+}, finally={
+	sink(NULL)
+})

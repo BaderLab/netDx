20a69d312c15b0b159606ecf33c3940995507e32
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-04-10 13:34:03 -0500

    general, will now run for all pancancer types
    Former-commit-id: 532412cc20755bbd5137170f8fcd7e42cd0b24df
    Former-commit-id: d8c891517bf8f675ee932f6aba6a5dc384050071

--- misc/PanCancer/featureEval/plotConsResults.R
+++ misc/PanCancer/featureEval/plotConsResults.R
@@ -2,35 +2,50 @@
 
 require(ROCR)
 
-inDir <- "/mnt/data2/BaderLab/PanCancer_KIRC"
 
-dirSet <- list(
-		allCons=sprintf("%s/output/consNets_170410",inDir),
-		randNets=sprintf("%s/output/randomNets_170410",inDir),
-		bestCons=sprintf("%s/output/bestConsNets_170410",inDir)
-	)
+# rootDir <- "/mnt/data2/BaderLab" # VM1
+rootDir <- "/home/netdx/BaderLab" # VM4
 
-predSet <- list()
-for (d in names(dirSet)) {
-	fSet <- dir(sprintf("%s/predictions", dirSet[[d]]),pattern="prediction")
-	cat(sprintf("%s: Got %i predictions\n", d, length(fSet)))
-	val <- rep(NA, length(fSet))
-	for (fName in fSet) {	
-		dat <- read.delim(sprintf("%s/predictions/%s",dirSet[[d]],fName),
-			sep="\t",h=T,as.is=T)
-    	pred <- prediction(dat$SURVIVEYES_SCORE-dat$SURVIVENO_SCORE,    
-                          dat$STATUS=="SURVIVEYES")                             
-                                                                                
-		c1 <- "SURVIVEYES" #numc[1]                                     
-		tp <- sum(dat$STATUS==dat$PRED_CLASS & dat$STATUS == c1)        
-		tn <- sum(dat$STATUS==dat$PRED_CLASS & dat$STATUS != c1)        
-		fp <- sum(dat$STATUS!=dat$PRED_CLASS & dat$STATUS != c1)        
-		fn <- sum(dat$STATUS!=dat$PRED_CLASS & dat$STATUS == c1)        
+for (curSet in "OV") {
+	inDir <- sprintf("%s/PanCancer_%s",rootDir,curSet)
+	dirSet <- list(
+			allCons=sprintf("%s/output/consNets_170410",inDir),
+			randNets=sprintf("%s/output/randomNets_170410",inDir),
+			bestCons=sprintf("%s/output/bestConsNets_170410",inDir)
+		)
+	
+	predSet <- list()
+	for (d in names(dirSet)) {
+		fSet <- dir(sprintf("%s/predictions", dirSet[[d]]),
+			pattern="prediction")
+		cat(sprintf("%s: Got %i predictions\n", d, length(fSet)))
+		val <- rep(NA, length(fSet))
+		for (fName in fSet) {	
+			dat <- read.delim(sprintf("%s/predictions/%s",dirSet[[d]],fName),
+				sep="\t",h=T,as.is=T)
+	    	pred <- prediction(dat$SURVIVEYES_SCORE-dat$SURVIVENO_SCORE,    
+	                          dat$STATUS=="SURVIVEYES") 
 
-		# dummy score column needed for perfCalc()                      
-		tmp <- data.frame(score=0,tp=tp,tn=tn,fp=fp,fn=fn)              
-		val[k] <- performance(pred, "auc")@y.values[[1]]        
+			c1 <- "SURVIVEYES" #numc[1]                                     
+			tp <- sum(dat$STATUS==dat$PRED_CLASS & dat$STATUS == c1)        
+			tn <- sum(dat$STATUS==dat$PRED_CLASS & dat$STATUS != c1)        
+			fp <- sum(dat$STATUS!=dat$PRED_CLASS & dat$STATUS != c1)        
+			fn <- sum(dat$STATUS!=dat$PRED_CLASS & dat$STATUS == c1)        
+	
+			# dummy score column needed for perfCalc()                      
+			tmp <- data.frame(score=0,tp=tp,tn=tn,fp=fp,fn=fn)              
+			val[k] <- performance(pred, "auc")@y.values[[1]]        
+		}
+		predSet[[d]] <- val
 	}
-	predSet[[d]] <- val
+	pdf(sprintf("%s/PanCancer_common/%s_consRes_plot.pdf",rootDir,curSet))
+	
+	tryCatch({
+		boxplot(predSet)
+	},error=function(ex){
+		print(ex)
+	},finally={
+		dev.off()
+	})
 }
 

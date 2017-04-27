55e1418f2d049b7324e2a4efef14b7f14ed0fd7e
Shraddha Pai <shraddhapai.neuro@gmail.com>
2017-04-27 09:37:29 -0400

    remotely harvests results off all vms
    Former-commit-id: 5dde26e3435f3edb6ac052a7af5e1f17a2ffeb4f
    Former-commit-id: 823dba3d6fbacfe7a8d959d4859a5054a533249c

---
+++ misc/PanCancer/featSel_oneNetPer/getTarballed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# run on mac, remotely tarballs results and downloads
+# KIRC pathway predictor
+
+VM1=spai@192.168.81.125
+VM3=spai@192.168.81.249
+VM4=netdx@192.168.81.206
+
+localDir=/Users/shraddhapai/Documents/Research/BaderLab/2017_TCGA_KIRC/output/KIRC_oneNetPer_170425
+
+mkdir -p $localDir
+
+
+# VM4 - gets 89-100
+indir=/home/netdx/BaderLab/PanCancer_KIRC/output/featSel_oneNetPer_170426
+outF=KIRC_oneNetPer_VM4_170426.tar.gz
+ssh $VM4 "cd $indir; tar cvfz ~/${outF} rng*/predictionResults.txt rng*/SURVIVE*/*/*CV_score.txt" 
+scp $VM4:~/${outF} .
+
+mv *gz ${localDir}/.
+cd $localDir
+tar xvfz KIRC_oneNetPer_VM4_170426.tar.gz
+

feeae84b3ddcc4fad6824b6c92d724eaa8bd1e96
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-05-02 09:59:47 -0400

    moved code for predictor variations into own folders
    Former-commit-id: 38dff3d8bebf1f02ac5ac4c3f921d1c996960acf
    Former-commit-id: 97a84f6a23a418c82868bfbd2ab9d9d1c3746b33

--- misc/PanCancer/featSel_pathways/writeConsensusNets_oneSet.R
+++ misc/PanCancer/featSel_pathways/writeConsensusNets_oneSet.R
@@ -1,9 +1,47 @@
 # write names of nets that pass consensus across multiple runs
 require(netDx)
 
-writeConsensusNets <- function(datDir,consCutoff=7L,
-	predClasses=c("SURVIVEYES","SURVIVENO"),
-	pctPass=0.5,outPfx="./pred_") {
+#' @param datDir (char) path to output results directory. Directory structure
+#' should be:  <datDir>/rng<runNumber>/<className>/GM_results/*_pathway_CV_score.txt
+#' @param predClasses (char) classes that are being predicted
+#' @param consCutoff (integer) nets must pass this cutoff in at least 
+#' pctPass of the iterations to be written to file
+#' @param pctPass (numeric between 0 and 1) fraction of iterations that
+#' a net's score must pass consCutoff for, to be included in the consensus
+#' map
+#' @param netInfo (list)
+#' keys are user-defined network types (e.g. clinical, mutations, expression),
+#' value is a list with the entry:
+#' 1) pattern: (char) a unique pattern to grep networks of this type in 
+#' pathway_CV_score.txt. See note with ***.
+#' 2) netSet: (list or char) key-value pair of network names and included units. 
+#' e.g. pathway names and member genes.
+#' If set to "base", the network name is written as is to the output file
+#' *** Order in this list matters. If a net category does not have a unique
+#' pattern, list it after others that do. Then those nets will be first
+#' accounted for, and the residuals will be grep-ed for with this pattern.
+#' For example, if mutation nets are MUT_*_cont and clinical nets have *_cont
+#' then list the mutation ones first. After these are removed, any with *_cont
+#' remaining should be clinical.
+#' @param outPfx (char) prefix to output filename (absolute path)
+#' @param trimFromName (char) strings to trim from net names before writing
+#' to file
+#' @return no values. 
+#' Writes several files with consensus/all net info
+#' Outdir is <outPfx>_<gp>_thresh<i>_pctPass<1.2f>
+#' 1) outDir/AllNets.txt  : All input nets
+#' 2) outDir/consNets.txt : Consensus nets for cutoff
+#' 3) outDir/netScores.txt : Table with net-level score
+#' for all iterations provided
+#' 4) outDir/consensus.gmt
+#' 5) outDir/pheno.txt
+#' 6) outDir/PSN.txt
+#' 7) outDir/netInfo.txt
+writeConsensusNets <- function(datDir,
+	predClasses=c("SURVIVEYES","SURVIVENO"),netInfo,
+	consCutoff=7L,pctPass=0.5,outPfx="./pred_") {
+
+	outPfx <- sprintf("%s_thresh%i_pctPass%1.2f", outPfx,consCutoff, pctPass)
 	for (gp in predClasses) {
 		rngDirs <- dir(path=datDir, pattern="^rng")
 		rngDirs <- setdiff(rngDirs, rngDirs[grep("tar.gz",rngDirs)])

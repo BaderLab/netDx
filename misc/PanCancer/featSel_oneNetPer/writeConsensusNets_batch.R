886323a48aaf8e060eda64a4a62baee367a8b526
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-05-03 10:52:45 -0400

    updated dir for all datatypes
    Former-commit-id: b184a8d726a616adb3f12a0abced5a6951346630
    Former-commit-id: 0b2f88e20f36229bde9aaa03bbfc51d5cd5867f7

--- misc/PanCancer/featSel_oneNetPer/writeConsensusNets_batch.R
+++ misc/PanCancer/featSel_oneNetPer/writeConsensusNets_batch.R
@@ -5,11 +5,9 @@ rm(list=ls())
 rootDir <- "/Users/shraddhapai/Documents/Research/BaderLab"
 dirList <- list(
 	KIRC=sprintf("%s/2017_TCGA_KIRC/output/KIRC_oneNetPer_170426",rootDir),
-	GBM="",#sprintf("%s/2017_TCGA_GBM/output/featSel_incMut_round2_170223",
-		#rootDir),
-	OV="",#sprintf("%s/2017_TCGA_OV/output/OV_170227",rootDir),
-	LUSC="" #sprintf("%s/2017_TCGA_LUSC/output/featSel_incMutRPPA_round2170223",
-		#rootDir)
+	GBM=sprintf("%s/2017_TCGA_GBM/output/GBM_oneNetPer_170425",rootDir),
+	OV=sprintf("%s/2017_TCGA_OV/output/OV_oneNetPer_170425",rootDir),
+	LUSC=sprintf("%s/2017_TCGA_LUSC/output/LUSC_oneNetPer_170425",rootDir)
 )
 
 source("writeConsensusNets_oneSet.R")

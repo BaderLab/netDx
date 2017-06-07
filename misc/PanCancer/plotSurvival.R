82bcdf1168291101e0528b3f879bba7a4856ec26
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-06-07 11:23:31 -0400

    prints medians of low/high groups
    Former-commit-id: 2c065231f1a109027b8826f52d6de8035dd793ef
    Former-commit-id: 52f79b90e6702cb5d57426f9948b38b41c779ff0

--- misc/PanCancer/plotSurvival.R
+++ misc/PanCancer/plotSurvival.R
@@ -16,11 +16,17 @@ for (k in inSet) {
 		inRoot,k,k),sep="\t",h=T,as.is=T)
 	x <- merge(x=surv,y=pheno,by="feature")
 	x$OS_years <- x$OS_OS/365
+	x$OS_mos <- x$OS_OS/30
 	p <- ggplot(x,aes(factor(is_alive),OS_years)) + geom_boxplot()
 	p <- p + geom_hline(yintercept=pCutoff[[k]],col='red')
 	p2 <- wilcox.test(x$OS_years[which(x$is_alive<1)],
 										x$OS_years[which(x$is_alive>0)],"less")$p.value
 	p <- p + ggtitle(sprintf("%s: p < %1.2e",k,p2))
+
+	mu1 <- median(x$OS_mos[which(x$is_alive<1)])
+	mu2 <- median(x$OS_mos[which(x$is_alive>0)])
+	cat(sprintf("%s: Low = %1.1f mos; High = %1.1f mos; diff= %1.1f YEARS\n",
+		k,mu1,mu2,(mu2-mu1)/12))
 	pList[[k]] <- p
 }
 

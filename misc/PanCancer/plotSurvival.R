1d54faf173c7334afe3dd1e235c7aef00031b787
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-06-07 10:05:23 -0400

    plotting survival
    Former-commit-id: 502b415cc4f506ef45f5f94b0b354982b4fe1a3a
    Former-commit-id: a8eb7b135884f7920fda8c0ad9491c4013cd66ef

---
+++ misc/PanCancer/plotSurvival.R
@@ -0,0 +1,28 @@
+#' plot survival boxplot
+
+inRoot <- "/Users/shraddhapai/Dropbox/netDx/BaderLab/"
+
+inSet <- c("KIRC","LUSC","OV","GBM")
+
+pCutoff <- list(KIRC=4,LUSC=2,OV=3,GBM=1)
+
+require(ggplot2)
+dat <- list()
+pList <- list()
+for (k in inSet) {
+	surv <- read.delim(sprintf("%s/2017_TCGA_%s/input/%s_binary_survival.txt",
+		inRoot,k,k),sep="\t",h=T,as.is=T)
+	pheno <- read.delim(sprintf("%s/2017_TCGA_%s/input/%s_OS_core.txt",
+		inRoot,k,k),sep="\t",h=T,as.is=T)
+	x <- merge(x=surv,y=pheno,by="feature")
+	x$OS_years <- x$OS_OS/365
+	p <- ggplot(x,aes(factor(is_alive),OS_years)) + geom_boxplot()
+	p <- p + geom_hline(yintercept=pCutoff[[k]],col='red')
+	p2 <- wilcox.test(x$OS_years[which(x$is_alive<1)],
+										x$OS_years[which(x$is_alive>0)],"less")$p.value
+	p <- p + ggtitle(sprintf("%s: p < %1.2e",k,p2))
+	pList[[k]] <- p
+}
+
+source("multiplot.R")
+multiplot(plotlist=pList,layout=matrix(1:4,ncol=2))

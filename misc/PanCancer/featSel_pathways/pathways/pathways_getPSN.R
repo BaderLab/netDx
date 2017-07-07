224f0339e188dc095e3a9e3545ea9ac1fa75d9c8
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-07-07 11:07:00 -0400

    updated for new pathway run
    Former-commit-id: 004badb39f00b488e047355856ed1176188814b9
    Former-commit-id: 7f4757b17bf06ae0bbbf9da57179c90056e7eec2

--- misc/PanCancer/featSel_pathways/pathways/pathways_getPSN.R
+++ misc/PanCancer/featSel_pathways/pathways/pathways_getPSN.R
@@ -4,7 +4,7 @@ rm(list=ls())
 # data dirs for input
 rootDir <- "/Users/shraddhapai/DropBox/netDx/BaderLab"
 outDir <- sprintf("%s/2017_PanCancer_Survival/pathwaysOnly_170502",rootDir)
-dataDir <- sprintf("%s/2017_TCGA_KIRC/output/pathOnly_consNets_170509",
+dataDir <- sprintf("%s/2017_TCGA_KIRC/output/pathway_170502",
 	rootDir)
 inData <- list(
 setName="pathways",
@@ -14,25 +14,25 @@ survival_file=sprintf("%s/2017_TCGA_KIRC/input/KIRC_binary_survival.txt",
 	rootDir),
 outDir=outDir,
 ptFile=list(
-	YES=sprintf("%s/SURVIVEYES/tmp/GENES.txt",dataDir),
-	NO=sprintf("%s/SURVIVENO/tmp/GENES.txt",dataDir)
+	YES=sprintf("%s/rng2/SURVIVEYES/tmp/GENES.txt",dataDir),
+	NO=sprintf("%s/rng2/SURVIVENO/tmp/GENES.txt",dataDir)
 ),
 # net ID-to-name mappings
 netInfo=list(
-	YES=sprintf("%s/SURVIVEYES/tmp/NETWORKS.txt",dataDir),
-	NO=sprintf("%s/SURVIVENO/tmp/NETWORKS.txt",dataDir)
+	YES=sprintf("%s/rng2/SURVIVEYES/tmp/NETWORKS.txt",dataDir),
+	NO=sprintf("%s/rng2/SURVIVENO/tmp/NETWORKS.txt",dataDir)
 ),
 # interaction nets
 netDir=list(
-	YES=sprintf("%s/SURVIVEYES/tmp/INTERACTIONS",dataDir),
-	NO=sprintf("%s/SURVIVENO/tmp/INTERACTIONS",dataDir)
+	YES=sprintf("%s/rng2/SURVIVEYES/tmp/INTERACTIONS",dataDir),
+	NO=sprintf("%s/rng2/SURVIVENO/tmp/INTERACTIONS",dataDir)
 ),
 # we are going to take union of FS pathways for each class so we need
 # the pathway scores for each class
 netScoreFile=list(
-	YES=sprintf("%s/featSelNets/KIRC_thresh10_pctPass0.70_SURVIVEYES_netScores.txt",
+	YES=sprintf("%s/featSelNets/pathways_thresh10_pctPass0.70_SURVIVEYES_netScores.txt",
 	outDir),
-	NO=sprintf("%s/featSelNets/KIRC_thresh10_pctPass0.70_SURVIVENO_netScores.txt",
+	NO=sprintf("%s/featSelNets/pathways_thresh10_pctPass0.70_SURVIVENO_netScores.txt",
 	outDir)
 )
 )

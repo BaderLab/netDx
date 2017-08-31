b74b5a013c92aa183f86ad2becd58d4b681f7650
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-08-31 14:12:29 -0400

    functions to plot predictor results

---
+++ netDx/R/getFeatureScores.R
@@ -0,0 +1,80 @@
+#' Compile network scores into a matrix
+#'
+#' @details Given network scores over a set of trian/test splits, compiles these
+#' into a matrix for downstream analysis. See the section on "Output Files"
+#' @param inDir (char/list) directory containing directories with all split info
+#' or list of all CV score files.
+#' if inDir is a single directory then the expected format for CV score files is
+#' <inDir>/rngX/predClassX/GM_results/predClassX_pathway_CV_score.txt"
+#' if inDir is a list, it should have one key per class. The value should be the
+#' corresponding set of filenames for pathway_CV_score.txt
+#' @param predClasses (char) possible STATUS for patients
+#' @return (list) one key per patient class. Value is matrix of network
+#' scores across all train/test splits. Each score is the output of
+#' the inner fold of CV.
+#' @examples
+#' inDir <- sprintf("%s/inst/extdata/KIRC_example_data", 
+#' 		path.package("netDx_software_update"))
+#' all_rngs <- list.dirs(inDir, recursive = FALSE)
+#' all_classone_files <- unlist(lapply(all_rngs, function(x) 
+#' 		paste(x, "SURVIVEYES/GM_results/SURVIVEYES_pathway_CV_score.txt", 
+#'		sep = "/")))
+#'  all_classtwo_files <- unlist(lapply(all_rngs, function(x) 
+#' 		paste(x, "SURVIVENO/GM_results/SURVIVENO_pathway_CV_score.txt", 
+#' 		sep = "/")))
+#'  input_list <- list("SURVIVEYES" = all_classone_files,
+#'                      "SURVIVENO" = all_classtwo_files)
+
+#' net_scores <- genNetScores(inDir, c("SURVIVEYES","SURVIVENO"))
+#' @export
+getFeatureScores <- function(inDir,predClasses) {
+	if (missing(inDir)) stop("inDir not provided");
+	if (missing(predClasses)) 
+		stop("predClasses missing; please specify classes");
+
+	out <- list()
+	for (gp in predClasses) {
+		cat(sprintf("%s\n",gp))
+
+		if(class(inDir) == "character"){
+			cat("\tSingle directory provided, retrieving CV score files\n")
+			rngDirs <- dir(path=inDir, pattern="^rng")
+			fList <-sprintf("%s/%s/%s/GM_results/%s_pathway_CV_score.txt",
+					 inDir,rngDirs,gp,gp)
+		} else {
+			cat("\tList of filenames provided\n")
+			fList <- inDir[[gp]]
+		}
+
+		cat(sprintf("Got %i iterations\n", length(fList)))
+		netColl <- list()
+	
+		for (scoreFile in fList) {
+			tmp	 <- read.delim(scoreFile,sep="\t",h=T,as.is=T)
+			colnames(tmp)[1] <- "PATHWAY_NAME"
+				netColl[[scoreFile]] <- tmp
+		}
+
+			spos <- gregexpr("\\/",fList)
+			# get the name of the iteration (rngX) assuming directory structure
+			# rngX/<class>/GM_results>/pathway_CV_score.txt
+			fNames <- lapply(1:length(spos), function(x) {	
+				  n <- length(spos[[x]])
+					y <- substr(fList[x], spos[[x]][n-3]+1,spos[[x]][n-2]-1)
+					y
+			})
+			fNames <- unlist(fNames)
+			names(netColl) <- fNames
+	
+			# filter for nets meeting cutoff criteria
+			cat("* Computing consensus\n")
+			cons <- getNetConsensus(netColl); x1 <- nrow(cons)
+			na_sum <- rowSums(is.na(cons))
+			full_cons <- cons
+			cons <- cons[which(na_sum < 1),]
+	
+			out[[gp]] <- cons
+	}
+	return(out)
+}
+
---
+++ netDx/R/plotIntegratedPSN.R
@@ -0,0 +1,227 @@
+#' Visualize integrated patient similarity network based on selected features
+#'
+#' @details Generates a Cytoscape network where nodes are patients and edges
+#' are weighted by aggregate pairwise patient similarity.
+#' Integrated PSN plotting is intended to run 
+#' after feature selection, which identifies the subset of input networks
+#' predictive for each class of interest. The method of generating the network
+#' is as follows:
+#' All networks feature-selected in either patient category are concatenated; 
+#' where a network is feature-selected in both categories, it is included once.
+#' The similarity between two patients in the integrated network is the mean of
+#' corresponding pairwise similarities. Dissimilarity is defined as 
+#' 1-similarity, and Dijkstra distances are computed on this resulting network. 
+#' For visualization, only edges representing the top fraction of distances
+#' (strongest edge weights) are included.
+#' @param pheno (data.frame) sample metadata, requires ID and STATUS column.
+#' The status column will be used to retrieve patient class names
+#' @param baseDir (char) path to the results directory one level above class-
+#' specific interaction networks. 
+#' Standard nested cross-validation: <dataDir>/rng1 so the program finds
+#' <dataDir>/rng1/<classA>/tmp/INTERACTIONS/
+#' Single round of cross-validation: <dataDir>, so the program finds
+#' <dataDir>/<classA>/tmp/INTERACTIONS/
+#' @param netNames (list) keys are classes, and each value is a vector with
+#' the names of patient similarity networks to combine for the integrated 
+#' network. These should probably be feature-selected networks
+#' @param topX (numeric between 0 and 1) fraction of strongest edges to keep
+#' e.g. topX=0.2 will keep 20% top edges
+#' @param aggFun (char) function to aggregate edges from different PSN
+#' @param outDir (char) path to directory for tmp work
+#' @param verbose (logical) print detailed messages
+#' @return (list) information about the integrated network
+#  1) aggPSN_Full: (char) path to aggregated patient similarity network. This
+#' is the unpruned network and not the one displayed in Cytoscape. Edges are
+#' weighted by similarity
+#' 2) aggPDN_pruned: (char) path to file with pruned patient dissimilarity 
+#' network, derived by inverting edge weights of the net in aggPSN_Full and
+#' keeping the strongest edges.
+#' 2) incNets: (char) vector of networks used for integration
+#' 3) network_suid: (char) Cytoscape network identifier, so user may further
+#' 4) netPng: (char) path to png file with patient dissimilarity network
+#' created by Cytoscape
+#' modify the network
+#' @import EasycyRest 
+#' @import httr
+#' @import ggplot2
+#' @import RColorBrewer
+#' @export
+plotIntegratedPSN <- function(setName="predictor",pheno,baseDir,netNames,
+	topX=0.2, aggFun="MEAN",outDir=".",verbose=TRUE,...) {
+
+require(EasycyRest) # https://github.com/shraddhapai/EasycyRest
+require(httr)
+require(netDx)
+require(ggplot2)
+require(RColorBrewer)
+
+if (missing(pheno)) stop("pheno is missing.")
+if (missing(baseDir)) stop("baseDir is missing.")
+if (missing(netNames)) stop("netNames is missing.")
+
+predClasses <- unique(pheno$STATUS)
+colnames(pheno)[which(colnames(pheno)=="STATUS")] <- "GROUP"
+cat(sprintf("%i classes: { %s }\n", length(predClasses), paste(predClasses,
+	collapse=",")))
+
+# compile node/network locations for dataset
+ptFile 	<- list()
+netInfo <- list()
+netDir	<- list()
+for (cur in predClasses) {
+	ptFile[[cur]] <- sprintf("%s/%s/tmp/GENES.txt",baseDir,cur)
+	netInfo[[cur]]<- sprintf("%s/%s/tmp/NETWORKS.txt",baseDir,cur)
+	netDir[[cur]]	<- sprintf("%s/%s/tmp/INTERACTIONS", baseDir,cur)
+}
+
+# ------------------------
+# Set up Cytoscape
+styleName <- "PSNstyle"
+portNum <- 1234
+base.url <- sprintf("http://localhost:%i/v1",portNum)
+res <- NULL
+tryCatch({
+	res	<- httr::GET(sprintf("%s/styles",base.url))
+}, error=function(ex) {
+	 # if Cytoscape isn't launched we may want to launch it.
+	launchCytoscape()
+}, finally={
+	res	<- httr::GET(sprintf("%s/styles",base.url))
+})
+curStyles <- gsub("\\\"","",rawToChar(res$content))
+curStyles <- unlist(strsplit(curStyles,","))
+
+# throws warning if n < 3, ignore
+pal <- suppressWarnings(brewer.pal(name="Dark2",n=length(predClasses)))
+
+# create Cytoscape style for PSN
+if (any(grep("PSNstyle",curStyles))) {
+	cat("style exists not creating\n")
+} else {
+	cat("Creating style\n")
+	nodeFills <- map_NodeFillDiscrete("GROUP",predClasses,pal)
+	defaults <- list("NODE_SHAPE"="ellipse",
+			"NODE_SIZE"=50,
+			"EDGE_TRANSPARENCY"=120,
+			"EDGE_STROKE_UNSELECTED_PAINT"="#999999",
+			"NODE_TRANSPARENCY"=120)
+	sty <- createStyle(styleName,
+		defaults=defaults,
+		mappings=list(nodeFills))
+}
+
+# check: patient IDs should be the same for both classes
+if (length(ptFile)>=2) {
+for (k in 2:length(ptFile)) {
+	tmp <- system(sprintf("diff %s %s", ptFile[[1]],ptFile[[k]]),intern=TRUE)
+	if (!is.null(attr(tmp,"status"))){
+		cat(sprintf("Gene IDs for %s doesn't match that for %s.\n", predClass[k],
+				predClass[1]))
+		cat("These should be identical. Please check.\n")
+	}
+}
+
+# pool best nets for both
+poolDir <- sprintf("%s/pool",outDir)
+if (file.exists(poolDir)) unlink(poolDir,recursive=TRUE)
+dir.create(poolDir)
+	newNetIDs <- list()
+
+# pool feature selected nets from both groups
+alreadyAdded <- c()
+for (gps in names(ptFile)) {
+	cat(sprintf("Group %s\n", gps))
+	pTally <- netNames[[gps]]
+	pTally <- sub("_cont|\\.profile","",pTally)
+
+	netIDs <- read.delim(netInfo[[gps]],sep="\t",h=F,as.is=T)
+	netIDs[,2] <- sub("_cont|\\.profile","",netIDs[,2])
+
+	idx <- which(pTally %in% alreadyAdded)
+	if (any(idx)) {
+		if (verbose) 
+			cat(sprintf("Found a net already added before, removing: {%s}\n",
+				paste(pTally[idx],collapse=",")))
+		pTally <- pTally[-idx]
+	}
+
+	if (length(pTally)>=1) alreadyAdded <- c(alreadyAdded,pTally)
+
+	if (verbose) print(pTally)
+	curNetIds <- matrix(NA,nrow=length(pTally),ncol=2)
+	ctr <- 1
+
+	# copy feature selected nets for this group
+	for (cur in pTally) {
+		idx <- which(netIDs[,2] == cur)
+		if (length(idx)<1) {
+			cat(sprintf("%s: index not found!\n",cur))
+			browser()
+		}
+		netID <- sprintf("1.%s.txt",netIDs[idx,1])
+		tmp <- sprintf("%s.%s", gps,netID)
+		file.copy(from=sprintf("%s/%s", netDir[[gps]], netID),
+		to=sprintf("%s/1.%s",poolDir,tmp))
+
+		curNetIds[ctr,] <- c(sub(".txt","",tmp),
+		paste(gps,netIDs[idx,2],sep="."))
+		##cat(sprintf("\t%s -> %s\n", cur, tmp))
+		ctr <- ctr+1
+	}
+	newNetIDs[[gps]] <- curNetIds
+}
+
+# write compiled net info file
+newNetIDs <- do.call("rbind",newNetIDs)
+netInfo_combinedF <- sprintf("%s/netInfo.txt", poolDir)
+write.table(newNetIDs,file=netInfo_combinedF,sep="\t",
+col=F,row=F,quote=F)
+
+# aggregate
+aggNetFile <- netDx::writeWeightedNets(ptFile[[1]],
+	netInfo=netInfo_combinedF,
+	poolDir,keepNets=newNetIDs[,2],outDir,
+	filterEdgeWt=0,limitToTop=Inf,
+	plotEdgeDensity=FALSE,
+	writeAggNet=aggFun,verbose=FALSE)
+
+# read in aggregated similarity net and convert to dissimilarity for net view
+aggNet<- read.delim(aggNetFile,sep="\t",h=T,as.is=T)[,1:3]
+colnames(aggNet) <- c("AliasA","AliasB","weight")
+aggNet[,3] <- 1-aggNet[,3]  # convert similarity to dissimilarity
+
+}
+
+# create a pruned network for visualization
+aggNet_pruned <- netDx::pruneNetByStrongest(aggNet,pheno$ID, topX=topX)
+outFile <- sprintf("%s/%s_prunedNet_top%1.2f.txt",outDir,setName,topX)
+write.table(aggNet_pruned,file=outFile,sep="\t",col=TRUE,row=FALSE,
+	quote=FALSE)
+# layout network in Cytoscape
+network.suid <- EasycyRest::createNetwork(
+	nodes=pheno, nodeID_column="ID",edges=aggNet_pruned,
+	netName=sprintf("%s_%s_top%1.2f",setName,aggFun,topX),
+	collName="KIRC"
+)
+# spring-embedded layout on edge 'weight' column
+layout.url <- sprintf("%s/apply/layouts/kamada-kawai/%s?column=weight",
+	base.url,network.suid, sep="/")
+response <- httr::GET(url=layout.url)
+# apply style
+apply.style.url <- sprintf("%s/apply/styles/%s/%i",
+	base.url,styleName,network.suid)
+response <- httr::GET(apply.style.url)
+# export to png
+export.url <- sprintf("%s/networks/%s/views/first.png",
+	base.url,network.suid,sep="/")
+response <- httr::GET(export.url)
+pngFile <- sprintf("%s/outputPDN.png",outDir)
+writeBin(response$content, pngFile)
+
+out <- list(aggPSN_FULL=aggNetFile,aggPDN_pruned=outFile,
+		incNets=alreadyAdded,network_suid=network.suid,
+		netView=pngFile)
+
+return(out)
+}
+

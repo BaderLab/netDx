31ebe3e61d6c7315bcc75ba420e5440c893e9170
shraddhapai <shraddha.pai@utoronto.ca>
2018-01-26 18:25:32 -0500

    parse multicutoff preds
    Former-commit-id: ff03adfd871d494fd6d9264bbd9d5d89c71b5e4d
    Former-commit-id: 6c628989bdbb9a73d73165f19c2dcd898c3afe02

---
+++ misc/PanCancer/multiCutoff/LUSC_parseMulti.R
@@ -0,0 +1,31 @@
+#' plot LUSC results with multiple CV cutoffs
+rm(list=ls())
+
+#dataDir <- "/home/shraddhapai/BaderLab/2017_PanCancer/GBM/output/multiCutoff_180119"
+#dataDir <- "/home/shraddhapai/BaderLab/2017_PanCancer/LUSC/output/prune_180125"
+dataDir <- "/home/shraddhapai/BaderLab/2017_PanCancer/LUSC/output/pruneCheckIntegr_180126"
+#dataDir <- "/home/shraddhapai/BaderLab/2017_PanCancer/LUSC/output/prunePCA_180126"
+
+rngDir <- paste(sprintf("%s/rng",dataDir), 7:100,sep="")
+
+require(netDx)
+for (cutoff in 7:9) {
+	c7 <- sprintf("%s/clinicalAprot/cutoff%i/predictionResults.txt",rngDir,cutoff)
+	torm <- c()
+	for (idx in 1:length(c7)) {
+		dat <- read.delim(c7[idx],sep="\t",h=T,as.is=T)
+		x1 <- sum(dat$STATUS=="SURVIVEYES")
+		x2 <- sum(dat$STATUS=="SURVIVENO")
+	#	cat(sprintf("%i: %i YES, %i NO\n", idx,x1,x2))
+		if (x1<1 & x2<1) torm <- c(torm, idx)
+	}
+	cat(sprintf("%i: removing %i\n", cutoff,length(torm)))
+	if (length(torm)>0) c7 <- c7[-torm]
+	postscript(sprintf("LUSC_cutoff%i.eps",cutoff));
+	x <- plotPerf(c7,c("SURVIVEYES","SURVIVENO"))
+	y <- unlist(lapply(x,function(i) i$auroc))
+	cat(sprintf("%i, mean auroc= %1.2f\n", cutoff, mean(y)))
+	dev.off()
+}
+
+###

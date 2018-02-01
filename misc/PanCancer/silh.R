0462e73d2159b1eab2e50e0f10f3cc5089d26c94
shraddhapai <shraddha.pai@utoronto.ca>
2018-02-01 12:25:43 -0500

    made plotting optional

--- misc/PanCancer/silh.R
+++ misc/PanCancer/silh.R
@@ -3,9 +3,11 @@
 #' @param m (data.frame or matrix) rows are measures, columns are samples
 #' @param meas (char) dissimilarity measure. [Pearson]
 #' @param pal (char) RColorBrewer palette to use
-#' @pararm title (char) silhouette plot title
+#' @param title (char) silhouette plot title
+#' @param plotMe (logical) if true, plots silhouette plot
 #' @returns output of cluster::silhouette(). Side effect of plotting
-silh <- function(groups,m, meas="Pearson",pal="Dark2",title="") {
+silh <- function(groups,m, meas="Pearson",pal="Dark2",title="",
+	plotMe=TRUE) {
 
 if (class(groups)=="numeric") groups <- as.integer(groups)
  if (class(groups)!="factor") groups <- factor(groups)
@@ -22,7 +24,7 @@ if (meas=="Pearson") {
 require(RColorBrewer)
 si <- silhouette(as.integer(groups), d)
 pal <- suppressWarnings(brewer.pal(name=pal,n=length(levels(groups))))
-plot(si,col=pal[groups],main=title)
+if (plotMe) plot(si,col=pal[groups],main=title)
 #print(summary(si))
 return(si)
 

45fadf9eec796283458e27123646b239d37d7ad9
shraddhapai <shraddha.pai@utoronto.ca>
2018-05-03 06:39:22 -0400

    added includeAllNodes flag to force test patients to be in net
    Former-commit-id: f9fb5ac0dd013cbb7cec2e40838e16c4e3b39625
    Former-commit-id: 34bbfefef447106a40ab1c4c11a03efe5e94e6ce

--- netDx/R/sparsify2.R
+++ netDx/R/sparsify2.R
@@ -8,40 +8,63 @@
 #' @param cutoff (numeric) edges with weight smaller than this are set to NA
 #' @param maxInt (numeric) max num edges per node. 
 #' @param EDGE_MAX (numeric) max num edges in network
+#' @param includeAllNodes (logical) if TRUE, ensures at least one edge is present for each patient. This feature is required when sparsification excludes test patients that are required to be classified. If the sparsification rules exclude all edges for a patient and this flag is set, then the strongest edge for each missing patient is added to the net. Note that this condition results in the total number of edges potentially exceeding EDGE_MAX
 #' @return writes SIF content to text file (node1,node2,edge weight)
 #' @import reshape2
 #' @export
-sparsify2 <- function(W, outFile="tmp.txt",cutoff=0.3,maxInt=50,EDGE_MAX=1000)  {
+sparsify2 <- function(W, outFile="tmp.txt",cutoff=0.3,maxInt=50,EDGE_MAX=1000,
+	includeAllNodes=TRUE)  {
+
 	if (maxInt > ncol(W)) maxInt <- ncol(W)
 
    diag(W) <- 0;
 	 W[W < cutoff] <- NA
 	x <- list()
-	for (i in 1:nrow(W)) { x[[i]] <- sort(W[i,],decreasing=TRUE,na.last=NA)}
+	for (i in 1:nrow(W)) { x[[i]] <- sort(W[i,],decreasing=TRUE,na.last=TRUE)}
 	names(x) <- rownames(W)
 	 for (k in 1:length(x)) {
 			cur <- x[[k]]
 			tokeep <- names(cur)[1:min(length(cur),maxInt)]
 			W[k,which(!colnames(W)%in% tokeep)] <- NA
 		}
-	tmp <- na.omit(melt(W))
-	tmp <- tmp[order(tmp[,3],decreasing=TRUE),]
+	mmat <- na.omit(melt(W))
+	mmat <- mmat[order(mmat[,3],decreasing=TRUE),]
 
-	maxEdge <- nrow(tmp)
+	maxEdge <- nrow(mmat)
 	if (maxEdge>EDGE_MAX) maxEdge <- EDGE_MAX
-	tmp <- tmp[1:maxEdge,]
-	write.table(tmp,file=outFile,sep="\t",col=F,row=F,quote=F)
+	mmat <- mmat[1:maxEdge,]
+
+	# we should guarantee an edge from all patients- in this case
+	# the edge_max would be violated unless we come up with a better rule
+	if (includeAllNodes) {
+		mmat[,1] <- as.character(mmat[,1])
+		mmat[,2] <- as.character(mmat[,2])
+		univ <- c(mmat[,1],mmat[,2])
+		missing <- setdiff(rownames(W), univ)
+		cat(sprintf("missing = { %s }\n",paste(missing, collapse=",")))
+		if (length(missing)>0) {
+			cat(sprintf("Sparsify2: found %i missing patients; adding strongest edge\n",
+				length(missing)))
+			for (k in missing) { # add the strongest edge for the patient
+				tmp <- x[[k]]
+				mmat <- rbind(mmat, c(k, names(tmp)[1],tmp[1]))
+			}
+		}	
+	}
+	head(mmat)
+	write.table(mmat,file=outFile,sep="\t",col=F,row=F,quote=F)
+	return(mmat)
 
 ### the code below converts the SIF format back to a matrix,potentially
 ### for debugging. 
-###	W2 <- dcast(tmp,Var2~Var1,value.var="value")
+###	W2 <- dcast(mmat,Var2~Var1,value.var="value")
 ###	rownames(W2) <- W2[,1]; W2 <- W2[,-1]
 ###	W2 <- W2[,colnames(W)]
 ###	W2 <- W2[colnames(W),]
 ###	n <- ncol(W);
-###	sp <- nrow(tmp)/(n*(n-1))/2
+###	sp <- nrow(mmat)/(n*(n-1))/2
 ###	cat(sprintf("%i -> %i edges (%i%% sparsity)\n",
-###		sum(!is.na(W)), nrow(tmp), round(sp*100)))
+###		sum(!is.na(W)), nrow(mmat), round(sp*100)))
 ###   return(W2);
 }
 

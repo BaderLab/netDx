b008e62711a54527817ddbc124c27b90f01f76d3
shraddhapai <shraddha.pai@utoronto.ca>
2018-05-14 14:59:26 -0400

    writes stats to file
    Former-commit-id: 6362dd9d88baf748657d805556850b9ad9fb29ba
    Former-commit-id: d87d92593da3bfa896587d5d40af6715b89c9f1f

--- misc/PanCancer/multiCutoff/compileRes.R
+++ misc/PanCancer/multiCutoff/compileRes.R
@@ -11,22 +11,41 @@ rm(list=ls())
 }
 
 source("KIRC_getRes.R")
+dt <- format(Sys.Date(),"%y%m%d")
+
 kirc <- KIRC_getRes()
+tmp <- melt(do.call("rbind",kirc))
+tmp <- cbind(tmp,"KIRC")
+write.table(tmp,file=sprintf("KIRCstats_%s.txt",dt),sep="\t",col=T,row=T,quote=F)
+rm(tmp)
+
 kirc2 <- .getBest(kirc)
 kirc2 <- cbind(kirc2,"KIRC")
 
 source("GBM_getRes.R")
 gbm <- GBM_getRes()
+tmp <- melt(do.call("rbind",gbm))
+tmp <- cbind(tmp,"GBM")
+write.table(tmp,file=sprintf("GBMstats_%s.txt",dt),sep="\t",col=T,row=T,quote=F)
+rm(tmp)
 gbm2 <- .getBest(gbm)
 gbm2 <- cbind(gbm2,"GBM")
 
 source("OV_getRes.R")
 ov <- OV_getRes()
+tmp <- melt(do.call("rbind",ov))
+tmp <- cbind(tmp,"OV")
+write.table(tmp,file=sprintf("OVstats_%s.txt",dt),sep="\t",col=T,row=T,quote=F)
+rm(tmp)
 ov2 <- .getBest(ov)
 ov2 <- cbind(ov2,"OV")
 
 source("LUSC_getRes.R")
 lusc <- LUSC_getRes()
+tmp <- melt(do.call("rbind",lusc))
+tmp <- cbind(tmp,"LUSC")
+write.table(tmp,file=sprintf("LUSCstats_%s.txt",dt),sep="\t",col=T,row=T,quote=F)
+rm(tmp)
 lusc2 <- .getBest(lusc)
 lusc2 <- cbind(lusc2,"LUSC")
 
@@ -35,7 +54,6 @@ comb <- as.data.frame(comb)
 rownames(comb)<- NULL
 comb[,1] <- as.numeric(as.character(comb[,1]))
 colnames(comb) <- c("val","method","datatype","tumour")
-dt <- format(Sys.Date(),"%y%m%d")
 write.table(comb,file=sprintf("netDx_bestModel_%s.txt",dt),sep="\t",col=T,row=F,quote=F)
 
 # convert into table form for comparison with Yuan et al.

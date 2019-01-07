4fbe9a7e5601e575bb562725bbfebcb59cd895cc
shraddhapai <shraddha.pai@utoronto.ca>
2019-01-07 11:42:51 -0500

    now removes E with circumflex

--- netDx/R/cleanPathwayName.R
+++ netDx/R/cleanPathwayName.R
@@ -17,6 +17,7 @@ cleanPathwayName <- function(curP) {
 	pforfile	<- gsub("\\\354","X",pforfile)
 	pforfile	<- gsub("\\\302\\\240","_",pforfile)
 	pforfile	<- gsub("\\\240","X",pforfile)
+	pforfile	<- gsub("\\\312","_",pforfile)
 	pforfile	<- gsub("\\+","plus",pforfile)
 
 	return(pforfile)

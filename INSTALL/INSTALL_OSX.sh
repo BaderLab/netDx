db425cd4fff17a879be78e8dbea042a0904d1b22
Shraddha Pai <shraddha.pai@utoronto.ca>
2019-05-22 12:58:30 -0400

    Checks for all dependencies, only installs if not found
    Former-commit-id: 37c1a20e37c00a89569447a7574f82a79f7b9afe
    Former-commit-id: 8111f90c148491a1549f2f8ffbfdc53cfbc61f1f

--- INSTALL/INSTALL_OSX.sh
+++ INSTALL/INSTALL_OSX.sh
@@ -2,11 +2,68 @@
 
 # Script to automate dependency install for netDx
 
+echo "* Checking if Java installed ..."
+if java -version 2>&1 > /dev/null |  grep -q "java version" ; then
+  echo -e "\tdone."
+else {
+  echo -e "*** ERROR: Java not found; install (https://www.java.com/en/download/) or add to path"
+  exit 0;
+ }
+fi
+
+echo "* Checking if Python installed ..."
+if [[ $(python --version 2>&1)  ]]
+    then
+        echo -e "\tdone"
+    else {
+        echo -e "*** ERROR: Python not found; install (https://www.python.org/downloads/) or add to path"
+	exit 0;
+	}
+fi
+
+echo "* Checking if R installed ..."
+if R --version | grep -q "R version" ;  
+    then
+			 ver=`R --version | grep "R version" | cut -f 3 -d " "`
+			echo -e "\tversion found: $ver"
+	 	   ver1=`echo $ver | cut -f1 -d"."`
+		   ver2=`echo $ver | cut -f2 -d"."`
+			if [ $ver1 -ge 3 ] &&  [ $ver2 -ge 6 ]; then
+        echo -e "\tdone"
+		  else {
+				echo ""
+				echo -e "\t*** ERROR: Version 3.6+ of R required. Install from https://cran.r-project.org/, or add to path"
+			  exit 0
+		}
+			fi
+    else {
+				echo -e "\t*** ERROR: R not found. Install R 3.6+ from https://cran.r-project.org/, or add to path"
+	exit 0;
+	}
+fi
+
 # install R packages
+echo "* Installing R dependencies"
 echo "r <- getOption('repos'); r['CRAN'] <- 'http://cran.us.r-project.org'; options(repos = r);" > ~/.Rprofile
-Rscript -e "install.packages(c('devtools','curl','bigmemory','foreach','combinat','doParallel','ROCR','pracma','RColorBrewer','reshape2','ggplot2', 'caroline', 'rmarkdown','igraph','glmnet'))"
+
+declare -a PKGS=( devtools curl bigmemory foreach combinat doParallel ROCR pracma RColorBrewer reshape2 ggplot2 caroline rmarkdown igraph glmnet );
+for p in ${PKGS[@]};do 
+	echo -e "\t* Checking for $p"
+	Rscript -e "if(!requireNamespace(\"$p\",quietly=TRUE)){ install.packages(\"$p\")}"
+done
+
+echo "* Installing BioConductor if required"
 Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)){install.packages("BiocManager")}'
-Rscript -e 'if (!requireNamespace("GenomicRanges",quietly=TRUE)) { BiocManager::install("GenomicRanges")}'
-Rscript -e 'if (!requireNamespace("RCy3",quietly=TRUE)) { BiocManager::install("RCy3")}'
+
+echo "* Installing BioConductor dependencies if required"
+declare -a PKGS=( GenomicRanges RCy3 );
+for p in ${PKGS[@]};do 
+	echo -e "\t* Checking for $p"
+	Rscript -e "if(!requireNamespace(\"$p\",quietly=TRUE)){ BiocManager::install(\"$p\")}"
+done
+
+cd ..
+echo "* Installing netDx"
 R CMD INSTALL netDx
+echo "* Installing netDx.examples"
 R CMD INSTALL netDx.examples

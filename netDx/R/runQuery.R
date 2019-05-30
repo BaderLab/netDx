e9e9800837824dcc54d67fae215dcf057cb4173c
shraddhapai <shraddha.pai@utoronto.ca>
2019-05-30 14:11:28 -0400

    added flagg to suppress Java output
    Former-commit-id: 3d6c2ffc3bff88408f12db212843bde0e7de9cf9
    Former-commit-id: 0225390efa680f39b030a3cbd431c9dc56b45a85

--- netDx/R/runQuery.R
+++ netDx/R/runQuery.R
@@ -16,31 +16,26 @@
 #' runQuery(dbPath, GM_query,"/tmp")
 #' @export
 runQuery <- function(dbPath, queryFiles, resDir, verbose=TRUE,
-	JavaMemory=6L, numCores=1) {
+	JavaMemory=6L, numCores=1L) {
 	GM_jar	<- sprintf("%s/java/genemania-cytoscape-plugin-3.5.0.jar", path.package("netDx"))
 	qBase	<- basename(queryFiles[[1]][1])
 	logFile	<- sprintf("%s/%s.log", resDir, qBase)
 	cmd1	<- sprintf("java -d64 -Xmx%iG -cp %s org.genemania.plugin.apps.QueryRunner",JavaMemory*numCores,GM_jar)
 	queryStrings <- paste(queryFiles, collapse = ' ')
-	cmd2	<- sprintf(" --data %s --in flat --out flat --threads %i --results %s %s --netdx-flag true 2>&1 > %s",
-			dbPath, numCores, resDir, queryStrings, logFile)
+	cmd2	<- sprintf(" --data %s --in flat --out flat --threads %i --results %s %s --netdx-flag true",
+			dbPath, numCores, resDir, queryStrings)
 
 	cmd		<- paste(c(cmd1,cmd2),collapse=" ")
-	print(cmd)
+	if (!verbose) cmd <- sprintf("%s 2>1 /dev/null",cmd)
+	if (verbose) print(cmd)
 
 	  # file is not actually created - is already split in PRANK and NRANK segments on GeneMANIA side
 	resFile <- sprintf("%s/%s-results.report.txt", resDir,qBase)
-	attempt <- 1
-	# sometimes GM stochastically fails because of a failure-to-acquire-lock
-	# in the /dataset/user directory. SP attributes to a race condition
-	# when GM is executed in parallel. However, the problem did not seem
-	# to occur in Feb 2016 on the VM but does occur in April 2016. 
-	# This while-loop exists to ensure that all GM queries run.
-	# removed in version 1.1
 	t0	<- Sys.time()
-	system(cmd,wait=TRUE,ignore.stdout=!verbose, ignore.stderr=!verbose)
-	cat(sprintf("QueryRunner time taken: %1.1f s\n", Sys.time()-t0))
+	blah <- suppressWarnings(system(cmd,wait=TRUE,ignore.stdout=TRUE,
+		ignore.stderr=TRUE,intern=TRUE))
+	if (verbose) cat(sprintf("QueryRunner time taken: %1.1f s\n", Sys.time()-t0))
 
-	Sys.sleep(1)
+	Sys.sleep(3)
 	return(resFile)
 }

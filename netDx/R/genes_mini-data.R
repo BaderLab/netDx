71be690c8ee461e135a796689c9ee6c12687093d
shraddhapai <shraddha.pai@utoronto.ca>
2019-05-30 14:24:55 -0400

    moved

--- netDx/R/genes_mini-data.R
+++
@@ -1,9 +0,0 @@
-#' Example gene coordinates
-#'
-#' @docType data
-#'
-#' @usage data(genes_mini)
-#' 
-#' @format data.frame with RefSeq ID (name), chromosome (chrom),
-#' strand (strand), transcript start and end (txStart,txEnd) and
-#' HGNC symbol name (name2)

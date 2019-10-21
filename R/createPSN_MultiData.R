8d4b7e975329a9b2f6ddfb332446f4ba2e381de3
shraddhapai <shraddha.pai@utoronto.ca>
2019-10-21 14:45:09 -0400

    removed newline from message, suppressed output from system2 command
    Former-commit-id: 40428e31069bb4b113cf8ffb64006d8426dc3341
    Former-commit-id: 33aa45ff535e65f707a2bf2ad43484fadbaa82bd

--- R/createPSN_MultiData.R
+++ R/createPSN_MultiData.R
@@ -58,12 +58,12 @@ if (missing(customFunc)) stop("customFunc must be suppled.\n")
 
 # Filter for nets (potentially feature-selected ones)
 if (!is.null(filterSet)) {
-	if (verbose) message("\tFilter set provided\n")
+	if (verbose) message("\tFilter set provided")
 	groupList2 <- list()
 	for (nm in names(groupList)) {
 			idx <- which(names(groupList[[nm]]) %in% filterSet)
 			if (verbose) {
-				message(sprintf("\t\t%s: %i of %i nets pass\n",nm,
+				message(sprintf("\t\t%s: %i of %i nets pass",nm,
 				length(idx),length(groupList[[nm]])))
 			}
 			if (length(idx)>0) {

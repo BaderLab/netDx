d352fd1a2627852cb335a4515c692d3670cad2dc
shraddhapai <shraddha.pai@utoronto.ca>
2019-10-23 13:55:23 -0400

    1. no longer writes outupt files; no netDx.examples ref
    Former-commit-id: 2522f586f5f4126f00d29d9945912d7ec9f6b61f
    Former-commit-id: f16280dfc8d7fb5c4fed7f8a00ede1f12f4c233e

--- R/writeEmapInput.R
+++ R/writeEmapInput.R
@@ -18,33 +18,22 @@
 #' @param maxScore (integer) maximum possible score in one round of cross-
 #' validation. e.g. for 10-fold cross-validation, maxScore=10.
 #' @param trimFromName (char) strings to trim from name with sub()
-#' @param outPfx (char) prefix for output files. Should include directory
-#' name and prefix of file name.
 #' @param verbose (logical) print messages
-#' @return
-#' 1) <outPfx>.gmt file - for enrichment map
-#' 2) <outPfx>_nodeAttr.txt (file) table with node properties, notably type,
-#' pctPass
 #' @examples
-#' inDir <- sprintf("%s/extdata/KIRC_output", 
-#'	path.package("netDx.examples"))
-#' outDir <- paste(getwd(),"plots",sep="/")
+#' inDir <- sprintf("%s/extdata/example_output",path.package("netDx"))
+#' outDir <- paste(tempdir(),"plots",sep="/")
 #' if (!file.exists(outDir)) dir.create(outDir)
 #' featScores <- getFeatureScores(inDir,predClasses=c("SURVIVEYES","SURVIVENO"))
 #' gp <- names(featScores)[1]
-#' pathFile <- sprintf("%s/extdata/Human_160124_AllPathways.gmt",
-#'           path.package("netDx.examples"))
-#' pathwayList <- readPathways(pathFile)
+#' pathwayList <- readPathways(getExamplePathways())
 #' pathwayList <- pathwayList[c(1:5)]
-#' netInfoFile <- sprintf("%s/extdata/KIRC_output/inputNets.txt",
-#'      path.package("netDx.examples"))
-#' netTypes <- read.delim(netInfoFile,sep="\t",h=FALSE,as.is=TRUE)
+#' netInfoFile <- sprintf("%s/extdata/example_output/inputNets.txt",
+#'      path.package("netDx"))
 #' netInfo <- read.delim(netInfoFile,sep="\t",h=FALSE,as.is=TRUE)
-#' output_files <- writeEMapInput(featScores[[gp]],pathwayList,netInfo,
-#'                   outPfx=sprintf("%s/%s",outDir,gp))
+#' output_files <- writeEMapInput(featScores[[gp]],pathwayList,netInfo)
 #' @export
-writeEMapInput <- function(featScores, namedSets,netInfo,
-	outPfx=sprintf("%s/curr",tempdir()),pctPass=0.70,minScore=1,maxScore=10,
+writeEMapInput <- function(featScores, namedSets,netInfo,pctPass=0.70,
+	minScore=1,maxScore=10,
 	trimFromName=c(".profile","_cont"),verbose=FALSE) {
 	netNames <- featScores[,1];
 	featScores <- as.matrix(featScores[,-1])
@@ -72,23 +61,14 @@ writeEMapInput <- function(featScores, namedSets,netInfo,
 	colnames(netInfo) <- c("netType","netName")
 	df2 <- merge(x=df1,y=netInfo,by="netName")
 
-	# write node attributes
-	netAttrFile <- sprintf("%s_nodeAttrs.txt",outPfx)
-	write.table(df2,file=netAttrFile,sep="\t",col=TRUE,row=FALSE,quote=FALSE)
-
-	# write gmt
-	outFile <- sprintf("%s.gmt",outPfx)
-	conn <- file(outFile)
+	featSet <- list()
 	for (cur in df2$netName) {
 			k2 <- simpleCap(cur)
 			if (is.null(namedSets[[cur]])) namedSets[[cur]] <- k2
-
-			curr <- sprintf("%s\t%s\t%s\n", k2,k2,
-				paste(namedSets[[cur]],collapse="\t"))
-			writeLines(curr,conn)
+			featSet[[k2]] <- namedSets[[cur]]
 	}
-	close(conn)
+  out <- list(nodeAttrs=df2,featureSets=featSet)
 
-  return(c(outFile,netAttrFile))
+  return(out)
 }
 

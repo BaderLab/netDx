d0a81c966d58775ca5e72903ec96f03607a8df7c
Shraddha Pai <shraddha.pai@utoronto.ca>
2019-11-07 11:24:44 -0500

    works with cytoscape
    Former-commit-id: abf769168e4baa1b462e3df2d0485acfb22406cb
    Former-commit-id: e078a86e9794d9e099fe035e26a2681b1eaf696c

---
+++ vignettes/plotEmap_example.R
@@ -0,0 +1,88 @@
+suppressMessages(require(netDx))
+
+# -----------------------------------------------------------
+# prepare data
+# -----------------------------------------------------------
+cat("Preparing data\n")
+library(curatedTCGAData)
+library(MultiAssayExperiment)
+library(TCGAutils)
+curatedTCGAData(diseaseCode="BRCA", assays="*",dru.run=TRUE)
+
+brca <- curatedTCGAData("BRCA",c("mRNAArray","Mutation"),FALSE)
+
+# get subtype info
+pID <- colData(brca)$patientID
+pam50 <- colData(brca)$PAM50.mRNA
+pam50[which(!pam50 %in% "Luminal A")] <- "notLumA"
+pam50[which(pam50 %in% "Luminal A")] <- "LumA"
+staget <- colData(brca)$pathology_T_stage
+st2 <- rep(NA,length(staget))
+st2[which(staget %in% c("t1","t1a","t1b","t1c"))] <- 1
+st2[which(staget %in% c("t2","t2a","t2b"))] <- 2
+st2[which(staget %in% c("t3","t3a"))] <- 3
+st2[which(staget %in% c("t4","t4b","t4d"))] <- 4
+
+colData(brca)$ID <- pID
+colData(brca)$STATUS <- pam50
+idx <- union(which(pam50 == "Normal-like"), which(is.na(st2)))
+tokeep <- setdiff(pID, pID[idx])
+brca <- brca[,tokeep,]
+brca <- brca[,,1] # keep only clinical and mRNA data
+
+tmpDir <- tempdir()
+outDir <- paste(tmpDir,"results",sep="/")
+if (file.exists(outDir)) unlink(outDir,recursive=TRUE)
+dir.create(outDir)
+
+inDir <- sprintf("%s/extdata/example_output",
+                 path.package("netDx"))
+all_rngs <- list.dirs(inDir, recursive = FALSE)
+#print(head(basename(all_rngs)))
+
+dir(all_rngs[1])
+
+predClasses <- c("LumA","notLumA")
+featScores <- getFeatureScores(inDir,predClasses=c("LumA","notLumA"))
+dim(featScores[[1]])
+head(featScores[[1]][,1:3])
+
+pathwayList <- readPathways(getExamplePathways())
+xpr_genes <- rownames(assays(brca)[[1]])
+pathwayList <- lapply(pathwayList, function(x) x[which(x %in% xpr_genes)])
+
+netInfoFile <- sprintf("%s/inputNets.txt",inDir)
+netInfo <- read.delim(netInfoFile,sep="\t",h=TRUE,as.is=TRUE)
+
+Emap_res <- writeEMapInput_many(featScores,pathwayList,
+                                maxScore=10,pctPass=0.7,netInfo,verbose=TRUE)
+
+message("writing EMap")
+# write emap results to file)
+gmtFiles <- list()
+nodeAttrFiles <- list()
+
+for (g in names(Emap_res)) {
+   outFile <- sprintf("%s/%s_nodeAttrs.txt",outDir,g)
+   write.table(Emap_res[[g]][["nodeAttrs"]],file=outFile,
+               sep="\t",col=TRUE,row=FALSE,quote=FALSE)
+   nodeAttrFiles[[g]] <- outFile
+   
+   outFile <- sprintf("%s/%s.gmt",outDir,g)
+   conn <- base::file(outFile,"w")
+   tmp <- Emap_res[[g]][["featureSets"]]
+   gmtFiles[[g]] <- outFile
+   
+   for (cur in names(tmp)) {
+      curr <- sprintf("%s\t%s\t%s", cur,cur,
+                      paste(tmp[[cur]],collapse="\t"))
+      writeLines(curr,con=conn)
+   }
+   print(outFile)
+   close(conn)
+}
+
+plotEmap(gmtFiles[[1]],nodeAttrFiles[[1]])
+
+sessionInfo()
+
